91e67ee88ba719ccf059d72ea5964fcbb819a2d5
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-12 18:29:57 +0530

    blog routes created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import authRouter from "./routes/auth.routes.js"
+import blogRouter from './routes/blog.routes.js'
 import cookieParser from "cookie-parser"
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(cookieParser({
 }))
 
 app.use('/api/v1/auth', authRouter)
+
+app.use('/api/v1/blogs', blogRouter)
 
 app.get("/", (req, res) => {
     res.send("Welcome")
--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,5 +1,7 @@
 import express from "express"
+import { createBlog, getAllBlogs } from "../controllers/blog.Controllers.js";
 
-const router   = express.Router()
-
+const router = express.Router()
+router.get('/', getAllBlogs)
+router.post('/', createBlog)
 export default router;
