e524c9f04740f20012acbe4dc35962e5aa01ce06
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-14 11:01:56 +0530

    fixed bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,20 @@
 import express from "express";
 import cors from "cors"
-import authRouter from "./routes/auth.routes.js"
-import blogRouter from './routes/blog.routes.js'
+import authRoutes from "./routes/auth.routes.js"
+import blogRoutes from './routes/blog.routes.js'
 import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-app.use(cookieParser({
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
 }))
+app.use(cookieParser())
 
-app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/auth', authRoutes)
 
-app.use('/api/v1/blogs', blogRouter)
+app.use('/api/v1/blogs', blogRoutes)
 
 app.get("/", (req, res) => {
     res.send("Welcome")
