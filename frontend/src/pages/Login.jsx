43778f254bb133c2a6b5bb9fb83dff2b4a6b9496
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-14 13:20:23 +0530

    login component created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,1 +1,35 @@
-export default function
+import API from "../api/axios"
+
+
+
+export default function Login() {
+    const [formData, setFormData] = useState({
+        name: "",
+        password: ""
+    })
+
+    const handleInputChange = e => {
+        setFormData((prevData) => ({
+            ...prevData,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const handleInputChange = async e => {
+        e.preventDefault()
+        try {
+            const response = await API.post('/auth/login', formData)
+            console.log(response)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return (
+        <>
+            <form >
+                <input type="email" name="email" placeholder="email" value={formData.name} onChange={handleInputChange} />
+                <input type="password" name="password" placeholder="password" value={formData.password} onChange={handleInputChange} />
+                <button type="submit"  >Login</button>
+            </form>
+        </>
+    )
+}
