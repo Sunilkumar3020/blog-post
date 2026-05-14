d5d20bae4fab39eea2e2e41c6328603901b46d9b
Sunil kumar <sunilkumar3020@gmail.com>
2026-05-14 13:00:24 +0530

    register component created

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import API from "../api/axios"
+export default function Register() {
+    const [formData, setFormData] = useState({ name: '', email: '', password: '' })
+    const handleInputChange = e => {
+        setFormData((prevData) => (
+            {
+                ...prevData,
+                [e.target.name]: e.target.value
+            }
+        ))
+    }
+    const handleFormSubmit = async e => {
+        e.preventDefault();
+        try {
+            const response = await API.post('/auth/register', formData);
+            console.log(response)
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+    return (
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" name="name" placeholder="name" value={formData.name} onChange={handleInputChange} />
+                <input type="email" name="email" placeholder="email" value={formData.email} onChange={handleInputChange} />
+                <input type="password" name="password" placeholder="password" value={formData.password} onChange={handleInputChange} />
+            </form>
+        </>
+    )
+}
